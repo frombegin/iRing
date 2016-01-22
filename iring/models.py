a5e4e94b166afa39416f3fbe183dab97240a7a66
frombegin <frombegin@gmail.com>
2016-01-22 14:47:07 +0800

    common models

--- iring/models.py
+++ iring/models.py
@@ -4,49 +4,54 @@
 from django.db import models
 from django.conf import settings
 
+from core.models import TimestampedModel
+
 
 class CollectionManager(models.Manager):
-    @staticmethod
-    def create(user, name, description):
-        board = Collection()
-        board.user = user
-        board.name = name
-        board.description = description
-        board.save()
-        return board
+    def create(self, user, name, description):
+        return super().create(user=user, name=name, description=description)
 
 
-class Collection(models.Model):
+class Collection(TimestampedModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     name = models.CharField(max_length=120)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     objects = CollectionManager()
 
     def __str__(self):
         return "board[{}]: {} {}".format(self.id, self.name, self.description)
 
 
-class Item(models.Model):
+class Item(TimestampedModel):
     board = models.ForeignKey(Collection)
     title = models.CharField(max_length=120)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Item{}".format(self.simhash)
 
 
 class DocumentItem(Item):
     content = models.TextField()
+    simhash = models.CharField(max_length=16)
 
     def __str__(self):
         return "Document[{}]: {}, {}".format(self.id, self.title, self.content)
 
 
-class FileItem(Item):
+class AbstractFileItem(Item):
+    name = models.CharField(max_length=128)
     size = models.BigIntegerField()
+    sha1 = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
 
 
-class ImageItem(Item):
+class FileItem(AbstractFileItem):
+    pass
+
+
+class ImageItem(AbstractFileItem):
     width = models.IntegerField()
     height = models.IntegerField()
 
